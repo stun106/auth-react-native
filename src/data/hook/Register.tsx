8fc8c6b002ae95e0a92b4f28612f0941cce6d9ba
stun106 <bladingnordeste@gmail.com>
2024-10-06 01:09:35 -0300

    :sparkles: feat: hook para criar um usuario

--- src/data/hook/Register.tsx
+++ src/data/hook/Register.tsx
@@ -2,11 +2,13 @@ import { useCallback } from "react"
 import { RegisterService } from "../service/RegisterService"
 
 export const useRegister = () => {
-    const createUser = useCallback(async(registro: Register) => {
-        try {
-            const { status, data } = await RegisterService.createUsuario(registro);
-                if (status !== 200) throw new console.error('Erro ao enviar dados para api.');
-                
-        }
-    },[]) 
+    const createUser = useCallback(async (registro: Register) => {
+        const { status, data } = await RegisterService.createUsuario(registro);
+        if (status !== 201) throw new console.error('Erro ao enviar dados para api.');
+        return data;
+    }, [])
+
+    return {
+        createUser,
+    }
 }
